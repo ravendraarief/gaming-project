fb3089a906f2b492fb22ee21098e0e604f5a6553
ravendraarief <ravendraarief@gmail.com>
2021-06-04 13:46:02 +0700

    fix migrate typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
